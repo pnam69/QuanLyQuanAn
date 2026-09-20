5705b57f087a92c9d80a6abd37d0b5af41628df5
Hungkobtjhet <ffhungakira@gmail.com>
2026-09-20 16:19:31 +0700

    UI update

---
+++ QuanLyQuanAn/Migrations/20260914091240_UpdateDatabaseModels.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuanLyQuanAn.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDatabaseModels : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_NhanViens_MaNV",
+                table: "NhanViens",
+                column: "MaNV",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MonAns_MaMon",
+                table: "MonAns",
+                column: "MaMon",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DonHangs_MaDon",
+                table: "DonHangs",
+                column: "MaDon",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DanhMucs_MaDM",
+                table: "DanhMucs",
+                column: "MaDM",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_NhanViens_MaNV",
+                table: "NhanViens");
+
+            migrationBuilder.DropIndex(
+                name: "IX_MonAns_MaMon",
+                table: "MonAns");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DonHangs_MaDon",
+                table: "DonHangs");
+
+            migrationBuilder.DropIndex(
+                name: "IX_DanhMucs_MaDM",
+                table: "DanhMucs");
+        }
+    }
+}
